19e20472d34ea8f02ed0a54f8b2cff9813606854
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-02-07 18:04:06 +0100

    [UPDATE] clean

---
+++ src/main/java/com/ppcsamples/apisampler/interfaces/ISample.java
@@ -0,0 +1,11 @@
+package com.ppcsamples.apisampler.interfaces;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public interface ISample {
+    public ResponseEntity<?> uploadSample(MultipartFile file);
+
+}
---
+++ src/main/java/com/ppcsamples/apisampler/services/SampleService.java
@@ -0,0 +1,88 @@
+package com.ppcsamples.apisampler.services;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.ppcsamples.apisampler.interfaces.ISample;
+import com.ppcsamples.apisampler.metadata.SampleFormatEnum;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class SampleService implements ISample {
+    Logger logger = LoggerFactory.getLogger(SampleService.class);
+
+    @Override
+    public ResponseEntity<?> uploadSample(MultipartFile sample){
+
+        Map<String, Object> response = new HashMap<>();
+        try {
+            
+            String[] fileNameParts = sample.getOriginalFilename().split("\\.");
+
+
+            if ( fileNameParts.length <= 0) {
+                response.put("status", HttpStatus.BAD_REQUEST);
+                response.put("data", "Invalid file");
+                return ResponseEntity.badRequest().body(response);
+            }
+
+            String sampleExtension = fileNameParts[1];
+
+            SampleFormatEnum[] formats = SampleFormatEnum.values();
+            Stream<SampleFormatEnum> streamSampleFormatEnum = Arrays.stream(formats);
+
+            List<SampleFormatEnum> formatFound = streamSampleFormatEnum.filter(e -> e.toString().toLowerCase().contentEquals(sampleExtension))
+                    .collect(Collectors.toList());
+            
+            if ( formatFound.size() == 0 ) {
+                response.put("status", HttpStatus.BAD_REQUEST);
+                response.put("data", "Format not allowed");
+                return ResponseEntity.badRequest().body(response);
+            }
+
+           String formatConfirmed = formatFound.get(0).toString().toLowerCase();
+           UUID fileUuid = UUID.randomUUID();  
+           String fileNameToCreate =String.format("%s-%s.%s", fileNameParts[0], fileUuid.toString(), formatConfirmed);
+           String pathToCreate = String.format("%s/%s", System.getProperty("user.dir").replace("\\","/") + "/" + "samples", fileNameToCreate);
+           this.logger.info(String.format("Prepare new file : %s ", pathToCreate));
+
+           File sampleUpload = new File(pathToCreate);
+           if (sampleUpload.createNewFile()) {
+            this.logger.info(String.format("File is created : %s ", sampleUpload.getName()));
+            } else {
+                //System.out.println("File already exists.");
+            }
+            
+            if ( sampleUpload.canWrite() && sampleUpload.canRead() ) {
+                InputStream is = sample.getInputStream();
+                this.logger.info(String.format("Write buffer for : %s ", sampleUpload.getName()));
+                FileUtils.copyInputStreamToFile(is, sampleUpload);                
+            } else {
+                this.logger.info(String.format("Error while trying to access file (write or read) : %s ", sampleUpload.getName()));
+            }
+            response.put("status", HttpStatus.OK);
+            response.put("data", sampleUpload.getName());
+            return ResponseEntity.ok().body(response);
+
+        }    catch (Exception ex) {
+            logger.info(ex.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "unsupported media", ex);
+       }
+    }
+    
+}
